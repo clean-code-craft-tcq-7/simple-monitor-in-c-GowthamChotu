cc06c29be4359fcb879cacc04654ff862d174b9b
Gowtham <115538903+GowthamChotu@users.noreply.github.com>
2022-12-13 22:49:05 +0530

    Update BatteryMon.h

--- BatteryMon.h
+++ BatteryMon.h
@@ -1,3 +1,13 @@
 #include "TempMon.h"
 #include "StateOfChargeMon.h"
 #include "ChargeRateMon.h"
+
+int batteryIsOk(float temperature, float soc, float chargeRate) {
+  if((TemperatureIsOk(temperature)==0) || (StateofChargeIsOk(soc)==0) || (ChargeRateIsOk(chargeRate)==0))
+  {
+    return 0;
+  }
+  
+  return 1;
+  
+}
