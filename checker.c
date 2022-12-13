a2fa00a86efa255c7c5a512d085f414053799e68
Gowtham <115538903+GowthamChotu@users.noreply.github.com>
2022-12-13 22:50:07 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -3,16 +3,6 @@
 
 #include "BatteryMon.h"
 
-    
-int batteryIsOk(float temperature, float soc, float chargeRate) {
-  if((TemperatureIsOk(temperature)==0) || (StateofChargeIsOk(soc)==0) || (ChargeRateIsOk(chargeRate)==0))
-  {
-    return 0;
-  }
-  
-  return 1;
-  
-}
 
 int main() {
   assert(batteryIsOk(25, 70, 0.7));
