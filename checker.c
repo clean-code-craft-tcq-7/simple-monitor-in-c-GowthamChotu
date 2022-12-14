e2ad34cfd4b25f1580d9152e224a321ec417dfc6
Gowtham <115538903+GowthamChotu@users.noreply.github.com>
2022-12-14 13:45:56 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <assert.h>
 
-#define RangeCheck(ActualValue,MinRange,MaxRange) (ActualValue<=MinRange || ActualValue>=MaxRange)?0:1
+#define RangeCheck(ActlVal,MinVal,MaxVal) (ActVal<=MinRange || ActVal>=MaxVal)?0:1
 
-#define TempMinRange 0
-#define TempMaxRange 45
+#define TempMinVal 0
+#define TempMaxVal 45
 
-#define SocMinRange 20
-#define SocMaxRange 80
+#define SocMinVal 20
+#define SocMaxVal 80
 
 #define ChargeRateRange 0.8
 
@@ -17,8 +17,8 @@ int Check_ChargeRate(float chargeRate);
 int batteryIsOk(float temperature, float soc, float chargeRate)
 {
   int batterystate = 1;
-  batterystate =  RangeCheck(temperature,TempMinRange , TempMaxRange);
-  batterystate &= RangeCheck(soc, SocMinRange, SocMaxRange);
+  batterystate =  RangeCheck(temperature,TempMinVal , TempMaxVal);
+  batterystate &= RangeCheck(soc, SocMinVal, SocMaxVal);
   batterystate &= Check_ChargeRate(chargeRate);
   return batterystate;
 }
